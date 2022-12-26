404435886794d3eac86ff9cf710586501969f68a
Chuckleberry Finn <6071161+ChuckTheSheep@users.noreply.github.com>
2022-12-26 12:12:02 -0500

    Update bank-ISObjectClickHandler.lua

--- Contents/mods/pz-factionBanking/media/lua/server/bank-ISObjectClickHandler.lua
+++ Contents/mods/pz-factionBanking/media/lua/server/bank-ISObjectClickHandler.lua
@@ -3,11 +3,6 @@ require "ISObjectClickHandler"
 local ISObjectClickHandler_doClick = ISObjectClickHandler.doClick
 function ISObjectClickHandler.doClick(object, x, y)
     if not object then return end
-
     local factionBankID = object and object:getModData().factionBankID
-    if factionBankID then
-        bankWindow:onBrowse(object:getModData().factionBankID, object)
-    else
-        ISObjectClickHandler_doClick(object, x, y)
-    end
+    if not factionBankID then ISObjectClickHandler_doClick(object, x, y) end
 end
